c413736c6d6432a18aaa603aebb55f41d2880a7e
srustinaik <srustin47@gmail.com>
2025-11-22 13:01:28 +0530

    Update odd.py

--- odd.py
+++ odd.py
@@ -1,15 +1,19 @@
 import sys
 
-if len(sys.argv) == 2:
-    numbers = list(map(int, sys.argv[1].split(',')))
-    print("Numbers received from Jenkins parameter:", numbers)
+if len(sys.argv) < 2:
+    print("Usage: python even_odd.py <num1> <num2> <num3> ...")
     sys.exit(1)
-else:
-    numbers = [1, 2, 3, 4, 5, 6]
-    print("No Jenkins parameter provided. Using default numbers:", numbers)
 
-even_count = sum(1 for n in numbers if n % 2 == 0)
-odd_count = len(numbers) - even_count
+numbers = list(map(int, sys.argv[1:]))
+
+even_count = 0
+odd_count = 0
+
+for n in numbers:
+    if n % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
 
 print("Count of Even Numbers:", even_count)
-print("Count of Odd Numbers:", odd_count)
+print("Count of Odd Numbers:", odd_count)
